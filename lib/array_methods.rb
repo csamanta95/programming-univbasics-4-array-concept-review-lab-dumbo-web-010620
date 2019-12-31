2b8f837e5fd9df03b2efe28378105992571a4c6c
Samanta Chowdhury <csamanta95@gmail.com>
2019-12-31 01:35:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  max = 0
+  max = array[0]
       array.length.times do |count|
         if array[count] > max
           max = array[count]
